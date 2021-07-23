5de3fc7188bb6c53f37a8875a5ac0ea3cd1f9443
LioSergent <lio.sergent@gmail.com>
2021-07-23 16:39:21 +0200

    Improved jupyter notebook sync

--- scripts/nbsync.sh
+++ scripts/nbsync.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-ls
-pwd
+nb="$(basename "$1" .py).ipynb"
+echo "Syncing $1 with ipynb counterpart, running jupyter in background
+and selenium servers in background, to be run in folder where $1 is"
+jupyter notebook stop 8888
+gnome-terminal --working-directory=`pwd` -e "jupyter notebook --autoreload --NotebookApp.token=aaa"
+# Kernel might have to be changed, kind of weird how it behaves right now
+jupytext --set-kernel python3 --set-formats ipynb,py:percent --sync --execute $1
+jure --token=aaa --jupyter_root_dir=. --notebook_path=$nb
